f7492701352694c2352b1ec0253d356bb990b392
Jahidul Islam <jahid.info97@gmail.com>
2023-05-28 00:46:02 +0600

    initial comit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:wear_app/screens/multimedia_player.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wear Media Player',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MultiMediaPlayer(),
+    );
+  }
+}
---
+++ lib/screens/multimedia_player.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MultiMediaPlayer extends StatelessWidget {
+  const MultiMediaPlayer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
