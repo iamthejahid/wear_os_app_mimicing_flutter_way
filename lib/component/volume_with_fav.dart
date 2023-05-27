cdbd985f3273e555ecc98370a465a45a4d0f29bb
Jahidul Islam <jahid.info97@gmail.com>
2023-05-28 02:19:38 +0600

    update : shape aware design changes

--- lib/component/volume_with_fav.dart
+++ lib/component/volume_with_fav.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:wear/wear.dart';
 
 class VolumeAndFav extends StatelessWidget {
   const VolumeAndFav({super.key});
@@ -8,47 +9,53 @@ class VolumeAndFav extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          const SizedBox(
-            height: 24,
-            width: 24,
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                CircularProgressIndicator(
-                  value: 0.4,
-                  color: Colors.white,
-                  backgroundColor: Colors.grey,
-                  strokeWidth: 1.2,
-                ),
-                Icon(
-                  CupertinoIcons.speaker_2_fill,
-                  color: Colors.grey,
-                  size: 14,
-                )
-              ],
-            ),
-          ),
-          Container(
-            height: 24,
-            width: 24,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(
-                color: Colors.grey,
-                width: 1.2,
+      child: WatchShape(builder: (context, shape, _) {
+        bool isRound = shape == WearShape.round;
+        return Row(
+          mainAxisAlignment: isRound
+              ? MainAxisAlignment.center
+              : MainAxisAlignment.spaceBetween,
+          children: [
+            const SizedBox(
+              height: 24,
+              width: 24,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  CircularProgressIndicator(
+                    value: 0.4,
+                    color: Colors.white,
+                    backgroundColor: Colors.grey,
+                    strokeWidth: 1.2,
+                  ),
+                  Icon(
+                    CupertinoIcons.speaker_2_fill,
+                    color: Colors.grey,
+                    size: 14,
+                  )
+                ],
               ),
             ),
-            child: const Icon(
-              CupertinoIcons.heart,
-              color: Colors.grey,
-              size: 14,
-            ),
-          )
-        ],
-      ),
+            if (!isRound)
+              Container(
+                height: 24,
+                width: 24,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.2,
+                  ),
+                ),
+                child: const Icon(
+                  CupertinoIcons.heart,
+                  color: Colors.grey,
+                  size: 14,
+                ),
+              )
+          ],
+        );
+      }),
     );
   }
 }
