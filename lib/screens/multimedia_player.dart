4213ddbb715b822aa10e49046b9ddaf16f78534a
Jahidul Islam <jahid.info97@gmail.com>
2023-05-28 02:31:19 +0600

    update : ambient regarding ui update

--- lib/screens/multimedia_player.dart
+++ lib/screens/multimedia_player.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:square_percent_indicater/square_percent_indicater.dart';
+import 'package:wear/wear.dart';
 import 'package:wear_app/component/music_controller.dart';
 import 'package:wear_app/component/volume_with_fav.dart';
 
@@ -14,34 +15,40 @@ class _MultiMediaPlayerState extends State<MultiMediaPlayer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          SquarePercentIndicator(
-            height: 60,
-            width: 60,
-            progress: 0.6,
-            borderRadius: 8,
-            progressWidth: 3,
-            shadowColor: Colors.white,
-            progressColor: Colors.orange,
-            child: ClipRRect(
-              borderRadius: const BorderRadius.all(
-                Radius.circular(8),
-              ),
-              child: Image.asset("assets/alizee.jpg"),
-            ),
-          ),
-          Text(
-            "Moi… Lolita",
-            style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                  color: Colors.white,
+      body: AmbientMode(builder: (context, mode, _) {
+        bool isAmbient = mode == WearMode.ambient;
+        return Opacity(
+          opacity: isAmbient ? 0.6 : 1,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SquarePercentIndicator(
+                height: 60,
+                width: 60,
+                progress: 0.6,
+                borderRadius: 8,
+                progressWidth: 3,
+                shadowColor: Colors.white,
+                progressColor: isAmbient ? Colors.transparent : Colors.orange,
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(8),
+                  ),
+                  child: Image.asset("assets/alizee.jpg"),
                 ),
+              ),
+              Text(
+                "Moi… Lolita",
+                style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                      color: Colors.white,
+                    ),
+              ),
+              const MusicController(),
+              const VolumeAndFav()
+            ],
           ),
-          const MusicController(),
-          const VolumeAndFav()
-        ],
-      ),
+        );
+      }),
     );
   }
 }
